c79913d5eb3598c946a1cbb98184046b58f9f82f
the-luantran <the-luan.tran@mail.mcgill.ca>
2018-02-05 22:41:49 -0500

    prototype of Main Window ui

---
+++ src/app.py
@@ -0,0 +1,21 @@
+import sys
+from PyQt5.QtWidgets import QDialog, QApplication
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+from src.interface import MainWindow
+
+class AppWindow(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.window = QMainWindow()
+        self.ui = MainWindow.Ui_MainWindow()
+        self.ui.setupUi(self.window)
+        self.window.show()
+
+app = QApplication(sys.argv)
+# window = QMainWindow()
+# ui = MainWindow.Ui_MainWindow()
+# ui.setupUi(window)
+
+w = AppWindow()
+sys.exit(app.exec_())

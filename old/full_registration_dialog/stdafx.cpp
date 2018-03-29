d8c755cdc073fe4bd45ec77190278e983c6b4a30
the-luantran <the-luan.tran@mail.mcgill.ca>
2018-03-29 14:38:40 -0400

    removed old c++ code

--- old/full_registration_dialog/stdafx.cpp
+++
@@ -1,7 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-// full_registration_dialog.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
-
-

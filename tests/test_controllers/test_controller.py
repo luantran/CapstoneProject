c2199fa8eda0f1f3c2a8e6cd61a232afe40fed8c
Luan Tran <the-luan.tran@mail.mcgill.ca>
2018-11-16 17:32:20 -0500

    created pytest framework

---
+++ tests/test_controllers/test_controller.py
@@ -0,0 +1,6 @@
+import pytest
+
+class TestController(object):
+    def test_one(self):
+        x = 0
+        assert x == 0
---
+++ tests/test_readers/test_mri_reader.py
@@ -0,0 +1,6 @@
+import pytest
+
+class TestMRIReader(object):
+    def test_one(self):
+        x = 0
+        assert x == 0

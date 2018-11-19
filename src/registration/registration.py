e3be787692a60adea0201bce04580e6267393dca
abdullahsumbal <abdullahsumbal@gmail.com>
2018-11-18 17:29:55 -0800

    added design pattern strategy

--- src/registration/registration.py
+++ src/registration/registration.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+import vtk
 
 class Registration(ABC):
     def __init__(self):
@@ -11,3 +12,23 @@ class Registration(ABC):
     @abstractmethod
     def MRIXRayRegistration(self, mriLMReader, wrlLMReader, mriReader):
         pass
+
+    def AlignSurfaceLM(self, szeReader):
+        # Rotate surface because it is not aligned with landmarks
+        gen_trans = vtk.vtkGeneralTransform()
+        gen_trans.RotateZ(-90)
+        gen_trans.RotateX(90)
+
+        gen_trans_filter = vtk.vtkTransformPolyDataFilter()
+        gen_trans_filter.SetInputData(szeReader.actor.GetMapper().GetInput())
+        gen_trans_filter.SetTransform(gen_trans)
+
+        szeReader.actor.GetMapper().SetInputConnection(gen_trans_filter.GetOutputPort())
+        szeReader.actor.GetMapper().Update()
+
+    def ApplyTransform(self, actor, trans):
+        lm_trans = vtk.vtkTransformPolyDataFilter()
+        lm_trans.SetInputData(actor.GetMapper().GetInput())
+        lm_trans.SetTransform(trans)
+        actor.GetMapper().SetInputConnection(lm_trans.GetOutputPort())
+        actor.GetMapper().Update()
--- src/registration/rigidRegistration.py
+++ src/registration/rigidRegistration.py
@@ -7,45 +7,22 @@ class RigidRegistration(registration.Registration):
 
     def SurfaceXRayRegistration(self, szeLMReader, wrlLMReader, szeReader):
 
-        # # Sort Data by name
-        # surfaceLMData.sort(key=operator.itemgetter('name'))
-        # xrayLMData.sort(key=operator.itemgetter('name'))
-        #
-        # # matching points extraction
-        # landmarkXrayPositions = set()
-        # landmarkSurfacePositions = set()
-        #
-        #
-        # for data in xrayLMData:
-        #     landmarkXrayPositions.add(data['name'])
-        #
-        # for data in surfaceLMData:
-        #     landmarkSurfacePositions.add(data['name'])
-        #
-        # matchingPosition = landmarkSurfacePositions.intersection(landmarkXrayPositions)
-        #
-        # # Points to vtkPoints
-        # xrayLMPoints = vtk.vtkPoints()
-        # surfaceLMPoints = vtk.vtkPoints()
-        # for data in surfaceLMData:
-        #     if data['name'] in matchingPosition:
-        #         surfaceLMPoints.InsertNextPoint(data['x'], data['y'], data['z'])
-        #
-        # for data in xrayLMData:
-        #     if data['name'] in matchingPosition:
-        #         xrayLMPoints.InsertNextPoint(data['x'], data['y'], data['z'])
+        # Rotate surface because it is not aligned with landmarks
+        self.AlignSurfaceLM(szeReader)
 
-
-        # 1st register the topo to the xray using tps and external landmarks
-        # find the rigid registration using correspondances
+        # find the rigid registration using correspondences
         Transrigid = vtk.vtkLandmarkTransform()
         Transrigid.SetSourceLandmarks(szeLMReader.points)
         Transrigid.SetTargetLandmarks(wrlLMReader.capteurs_points)
         Transrigid.SetModeToRigidBody()
         Transrigid.Update()
-        szeReader.actor.SetUserTransform(Transrigid)
-        szeLMReader.actor.SetUserTransform(Transrigid)
-        return szeReader.actor, szeLMReader.actor
+
+        # Apply transformation to landmarks
+        self.ApplyTransform(szeLMReader.actor, Transrigid)
+
+        # Apply transformation to surface
+        self.ApplyTransform(szeReader.actor, Transrigid)
+
 
     def MRIXRayRegistration(self, mriLMReader, wrlLMReader, mriReader):
         Transrigid = vtk.vtkLandmarkTransform()
@@ -55,7 +32,6 @@ class RigidRegistration(registration.Registration):
         Transrigid.Update()
         mriReader.actor.SetUserTransform(Transrigid)
         mriLMReader.actor.SetUserTransform(Transrigid)
-        return mriReader.actor, mriLMReader.actor
 
 
 
